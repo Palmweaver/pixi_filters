d411e2e56e6e94fbd7cca39e97427842af9b25dd
Palmweaver <thepalmweaver@gmail.com>
2019-02-01 06:40:25 -0500

    Initial Commit

---
+++ pixi_filters/lib/pixi_filters.rb
@@ -0,0 +1,8 @@
+require "pixi_filters/version"
+
+module PixiFilters
+  class Error < StandardError; end
+  class Engine < ::Rails::Engine;
+  	
+  end
+end
---
+++ pixi_filters/lib/pixi_filters/version.rb
@@ -0,0 +1,3 @@
+module PixiFilters
+  VERSION = "0.1.0"
+end
